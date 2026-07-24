8c633446457b908f5f7306ef49710d244b656cff
Mia  Stubb-Olsen <miastubb@gmail.com>
2026-07-24 17:32:18 +0200

    chore: add temporary product mock data

---
+++ src/app/data/products.js
@@ -0,0 +1,17 @@
+const products = [
+  {
+    id: 1,
+    name: "25th Pokémon",
+    price: 2450,
+    image: "/images/product-1.png",
+  },
+  {
+    id: 2,
+    name: "Another product",
+    price: 1990,
+    image: "/images/product-2.png",
+  },
+
+];
+
+export default products;
