216a99d7bd1488dcae23d7f8355ca00a1ae0c280
Mia  Stubb-Olsen <miastubb@gmail.com>
2026-07-25 18:22:44 +0200

    fix product card responsiveness and add testing images

--- src/app/data/products.js
+++ src/app/data/products.js
@@ -1,17 +1,19 @@
+import testProduct1 from "../assets/testing-images/test-product-1.jpg";
+import testProduct2 from "../assets/testing-images/test-product-2.jpg";
+
 const products = [
   {
     id: 1,
     name: "25th Pokémon",
     price: 2450,
-    image: "/images/product-1.png",
+    image: testProduct1,
   },
   {
     id: 2,
     name: "Another product",
     price: 1990,
-    image: "/images/product-2.png",
+    image: testProduct2,
   },
-
 ];
 
 export default products;
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import products from "./data/products";
 export default function Home() {
   return (
     <main className="p-8">
-      <div className="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
