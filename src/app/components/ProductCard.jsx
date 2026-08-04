f97a4a87ec5047b56087a278b2d4f37884502bb0
Mia  Stubb-Olsen <miastubb@gmail.com>
2026-08-04 13:51:24 +0200

    changed text from text-sm to text-base

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -14,7 +14,7 @@ export default function ProductCard({ product }) {
       </div>
 
       <div className="bg-black p-3 text-white">
-        <h2 className="text-sm">{product.name}</h2>
+        <h2 className="text-base">{product.name}</h2>
 
         <div className="mt-4 flex items-end justify-between">
           <p className="text-2xl font-bold">
