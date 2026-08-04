74cc5fcfd134a1c4229f6b368dca8996605e75fb
Mia  Stubb-Olsen <miastubb@gmail.com>
2026-08-04 14:06:50 +0200

    added navigation from product cards to details page

--- src/app/components/ProductDetail.jsx
+++ src/app/components/ProductDetail.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 function ProductDetail({ product }) {
   return (
     <section className="mx-auto grid max-w-6xl grid-cols-1 gap-8 p-6 md:grid-cols-2">
-      <div className="flex items-center justify-center rounded-2xl border-2 border-yellow-400 bg-yellow-100 p-1">
+      <div className="flex aspect-square items-center justify-center rounded-2xl border-2 border-yellow-400 bg-yellow-100 p-1">
         <Image
           src={product.image}
           alt={product.name}
---
+++ src/app/product/[id]/page.js
@@ -0,0 +1,19 @@
+import { notFound } from "next/navigation";
+import ProductDetail from "../../components/ProductDetail";
+import products from "../../data/products";
+
+export default async function ProductPage({ params }) {
+  const { id } = await params;
+
+  const product = products.find((product) => product.id === Number(id));
+
+  if (!product) {
+    notFound();
+  }
+
+  return (
+    <main>
+      <ProductDetail product={product} />
+    </main>
+  );
+}
