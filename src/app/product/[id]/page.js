c583863130af5fbef638c4d5c434369f2e95eeb6
Mia  Stubb-Olsen <miastubb@gmail.com>
2026-08-09 20:45:24 +0200

    fixed import routing

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation";
-import ProductDetail from "../../components/ProductDetail";
-import products from "../../data/products";
+import ProductDetail from "../../../components/ProductDetail";
+import products from "../../../data/products";
 
 export function generateStaticParams() {
   return products.map((product) => ({
